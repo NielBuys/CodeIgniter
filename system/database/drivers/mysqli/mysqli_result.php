2fbe8d10e4e926f475dc11c20a6f976009be547e
Niel Buys <nbuys@ncomp.co.za>
2026-02-03 09:14:21 +0200

    Fix MYSQLI_TYPE_INTERVAL Deprecation Issue 32

--- system/database/drivers/mysqli/mysqli_result.php
+++ system/database/drivers/mysqli/mysqli_result.php
@@ -137,33 +137,39 @@ class CI_DB_mysqli_result extends CI_DB_result {
 	private static function _get_field_type($type)
 	{
 		static $map;
-		isset($map) OR $map = array(
-			MYSQLI_TYPE_DECIMAL     => 'decimal',
-			MYSQLI_TYPE_BIT         => 'bit',
-			MYSQLI_TYPE_TINY        => 'tinyint',
-			MYSQLI_TYPE_SHORT       => 'smallint',
-			MYSQLI_TYPE_INT24       => 'mediumint',
-			MYSQLI_TYPE_LONG        => 'int',
-			MYSQLI_TYPE_LONGLONG    => 'bigint',
-			MYSQLI_TYPE_FLOAT       => 'float',
-			MYSQLI_TYPE_DOUBLE      => 'double',
-			MYSQLI_TYPE_TIMESTAMP   => 'timestamp',
-			MYSQLI_TYPE_DATE        => 'date',
-			MYSQLI_TYPE_TIME        => 'time',
-			MYSQLI_TYPE_DATETIME    => 'datetime',
-			MYSQLI_TYPE_YEAR        => 'year',
-			MYSQLI_TYPE_NEWDATE     => 'date',
-			MYSQLI_TYPE_INTERVAL    => 'interval',
-			MYSQLI_TYPE_ENUM        => 'enum',
-			MYSQLI_TYPE_SET         => 'set',
-			MYSQLI_TYPE_TINY_BLOB   => 'tinyblob',
-			MYSQLI_TYPE_MEDIUM_BLOB => 'mediumblob',
-			MYSQLI_TYPE_BLOB        => 'blob',
-			MYSQLI_TYPE_LONG_BLOB   => 'longblob',
-			MYSQLI_TYPE_STRING      => 'char',
-			MYSQLI_TYPE_VAR_STRING  => 'varchar',
-			MYSQLI_TYPE_GEOMETRY    => 'geometry'
-		);
+
+		if (!isset($map)) {
+			$map = array(
+				MYSQLI_TYPE_DECIMAL     => 'decimal',
+				MYSQLI_TYPE_BIT         => 'bit',
+				MYSQLI_TYPE_TINY        => 'tinyint',
+				MYSQLI_TYPE_SHORT       => 'smallint',
+				MYSQLI_TYPE_INT24       => 'mediumint',
+				MYSQLI_TYPE_LONG        => 'int',
+				MYSQLI_TYPE_LONGLONG    => 'bigint',
+				MYSQLI_TYPE_FLOAT       => 'float',
+				MYSQLI_TYPE_DOUBLE      => 'double',
+				MYSQLI_TYPE_TIMESTAMP   => 'timestamp',
+				MYSQLI_TYPE_DATE        => 'date',
+				MYSQLI_TYPE_TIME        => 'time',
+				MYSQLI_TYPE_DATETIME    => 'datetime',
+				MYSQLI_TYPE_YEAR        => 'year',
+				MYSQLI_TYPE_NEWDATE     => 'date',
+				MYSQLI_TYPE_ENUM        => 'enum',
+				MYSQLI_TYPE_SET         => 'set',
+				MYSQLI_TYPE_TINY_BLOB   => 'tinyblob',
+				MYSQLI_TYPE_MEDIUM_BLOB => 'mediumblob',
+				MYSQLI_TYPE_BLOB        => 'blob',
+				MYSQLI_TYPE_LONG_BLOB   => 'longblob',
+				MYSQLI_TYPE_STRING      => 'char',
+				MYSQLI_TYPE_VAR_STRING  => 'varchar',
+				MYSQLI_TYPE_GEOMETRY    => 'geometry'
+			);
+
+			if (defined('MYSQLI_TYPE_INTERVAL')) {
+				$map[MYSQLI_TYPE_INTERVAL] = 'interval';
+			}
+		}
 
 		return isset($map[$type]) ? $map[$type] : $type;
 	}
